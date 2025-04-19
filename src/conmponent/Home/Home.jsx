51194c808f783183e545f41cae2cc10bb1772ea1
Ahmed90155 <93785943+Ahmed90155@users.noreply.github.com>
2025-04-19 17:46:33 +0200

    fix: handle 404 on refresh with vercel rewrites

--- src/conmponent/Home/Home.jsx
+++ src/conmponent/Home/Home.jsx
@@ -70,7 +70,7 @@ export default function Home() {
     <>
       <div className="min-h-screen">
         <CatogorySlider />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-10/12 m-auto mt-5 mb-10  ">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-10/12 m-auto mt-5 mb-10  ">
           {products.map((product) => (
             <motion.div
               initial={{ opacity: 0, y: 50 }}
