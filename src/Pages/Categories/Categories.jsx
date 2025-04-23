4c271c362bc161390b8721d87c6812a8cac74976
Ahmed90155 <93785943+Ahmed90155@users.noreply.github.com>
2025-04-23 08:27:44 +0200

    Replace old project with updated version

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,18 +1,15 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 import Slider from "react-slick";
-import Loading from '../../conmponent/Loading/Loading';
-
+import Loading from "../../conmponent/Loading/Loading";
 
 export default function Categories() {
-
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['Categories'],
-    queryFn: ()=> Client.categories.getAll(),
-    
+    queryKey: ["Categories"],
+    queryFn: () => Client.categories.getAll(),
   });
-if (isLoading) return <Loading/>
-      
+  if (isLoading) return <Loading />;
+
   if (isError) return <p>Something went wrong</p>;
   const settings = {
     dots: true,
@@ -22,45 +19,45 @@ if (isLoading) return <Loading/>
     autoplay: true,
     speed: 4000,
     autoplaySpeed: 4000,
-    cssEase: "linear"
+    cssEase: "linear",
   };
-const Categories = data?.data ??[]
+  const Categories = data?.data ?? [];
 
   return (
     <>
-     <div className="slider-container w-full mx-2 my-20">
-          <Slider {...settings}>
-            {Categories?.map((categorie) => 
-              <div key={categorie._id}>
-                <img src={categorie.image} alt={categorie.name} className="w-full h-40 object-contain mb-4" />
-                
-              </div>
-            )}
-            
-          </Slider>
-        </div>
-    
-    <div className="min-h-screen p-4">
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-    {Categories?.map((categorie) => (
-      <div
-        key={categorie._id}
-        className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col items-center p-4 hover:shadow-lg transition"
-      >
-        <img
-          src={categorie.image}
-          alt={categorie.name}
-          className="w-full h-40 object-contain mb-4"
-        />
-        <p className="text-lg font-medium text-center">{categorie.name}</p>
+      <div className="slider-container w-full mx-2 my-20">
+        <Slider {...settings}>
+          {Categories?.map((categorie) => (
+            <div key={categorie._id}>
+              <img
+                src={categorie.image}
+                alt={categorie.name}
+                className="w-full h-40 object-contain mb-4"
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
-    ))}
-  </div>
-</div>
 
-    
-    
-    
+      <div className="min-h-screen p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {Categories?.map((categorie) => (
+            <div
+              key={categorie._id}
+              className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col items-center p-4 hover:shadow-lg transition"
+            >
+              <img
+                src={categorie.image}
+                alt={categorie.name}
+                className="w-full h-40 object-contain mb-4"
+              />
+              <p className="text-lg font-medium text-center">
+                {categorie.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
